d236c5c6f35292cae4899cdb35af222ee6dbd3f2
Andrey Antukh <niwi@niwi.be>
2015-04-08 22:58:20 +0200

    Improve handlers communication functions.

--- src/catacumba/ratpack.clj
+++ src/catacumba/ratpack.clj
@@ -70,25 +70,39 @@
     (fn [chain] (reduce attach-route chain routes))
     {:type :ratpack-router}))
 
+(defn route-params
+  "Return a hash-map with parameters extracted from
+  routing patterns."
+  [^Context context]
+  (into {} utils/keywordice-keys-t (.getPathTokens context)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Handlers Communication
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defrecord ContextData [payload])
-
 (alter-meta! #'->ContextData assoc :private true)
 (alter-meta! #'map->ContextData assoc :private true)
 
 (defn delegate
-  "Delegate handling to the next handler in line."
+  "Delegate handling to the next handler in line.
+
+  This function accept an additiona parameter for
+  pass context parameters to the next handlers, and
+  that can be obtained with `context-params`
+  function."
   ([^Context context]
    (.next context))
   ([^Context context data]
    (let [reg (Registries/just (ContextData. data))]
      (.next context reg))))
 
-(defn context-data
-  "Get current context data."
+(defn context-params
+  "Get the current context params.
+
+  The current params can be passed to the next
+  handler using the `delegate` function. Is a simple
+  way to communicate the handlers chain."
   [^Context context]
   (try
     (let [cdata (.get context ContextData)]

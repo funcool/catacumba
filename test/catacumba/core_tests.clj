4366966e048f147ae65cd315957e89fd62917050
Andrey Antukh <niwi@niwi.be>
2015-04-08 22:58:58 +0200

    Add proper tests for ratpack style handlers.

--- test/catacumba/core_tests.clj
+++ test/catacumba/core_tests.clj
@@ -4,8 +4,6 @@
             [clojure.pprint :refer [pprint]]
             [clojure.core.async :as async]
             [clj-http.client :as http]
-            [ring.middleware.params :as ring-params]
-            [ring.middleware.keyword-params :as ring-kw-params]
             [catacumba.core :as ct]
             [catacumba.ratpack :as rp])
   (:import ratpack.registry.Registries))
@@ -20,52 +18,56 @@
        ~@body
        (finally (.stop server#)))))
 
-(defmacro with-router-server [handler & body]
-  `(let [handler# (-> ~handler
-                      (with-meta {:type :ratpack-router}))
-         server# (ct/run-server handler#)]
-     (try
-       ~@body
-       (finally (.stop server#)))))
-
 (def base-url "http://localhost:5050")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(deftest handler-with-string-return
-  (with-server (fn [_] "hello world")
-    (let [response (http/get base-url)]
-      (is (= (:body response) "hello world"))
-      (is (= (:status response) 200)))))
+(deftest request-response
+  (testing "Using send! with context"
+    (let [handler (fn [ctx] (rp/send! ctx "hello world"))]
+      (with-server (with-meta handler {:type :ratpack})
+        (let [response (http/get base-url)]
+          (is (= (:body response) "hello world"))
+          (is (= (:status response) 200))))))
 
-(defn simple-chained
-  [context params]
-  (println "in simple-chained")
-  (rp/delegate context {:data (rand-int 10000)}))
+  (testing "Using string as return value."
+    (let [handler (fn [ctx] "hello world")]
+      (with-server (with-meta handler {:type :ratpack})
+        (let [response (http/get base-url)]
+          (is (= (:body response) "hello world"))
+          (is (= (:status response) 200))))))
+)
 
-(defn hello-world-handler
-  [context params]
-  (let [data (rp/context-data context)]
-    (println "in hello-world-handler" data)
-    "hello world"))
+(deftest routing
+  (testing "Routing with parameter."
+    (let [handler (fn [ctx]
+                    (let [params (rp/route-params ctx)]
+                      (str "hello " (:name params))))
+          handler (rp/routes [[:get ":name" handler]])]
+      (with-server handler
+        (let [response (http/get (str base-url "/foo"))]
+          (is (= (:body response) "hello foo"))
+          (is (= (:status response) 200))))))
 
-(def myroutes
-  (rp/routes [[:prefix "static"
-               [:assets "public"]]
-              [:prefix "foo"
-               [:get ":name" simple-chained hello-world-handler]]]))
+  (testing "Routing assets with prefix."
+    (let [handler (rp/routes [[:prefix "static"
+                               [:assets "public"]]])]
+      (with-server handler
+        (let [response (http/get (str base-url "/static/test.txt"))]
+          (is (= (:body response) "hello world from test.txt\n"))
+          (is (= (:status response) 200))))))
 
-(deftest experiments
-  (with-router-server myroutes
-    (let [response (http/get (str base-url "/foo/bar"))
-          response2 (http/get (str base-url "/foo/bar"))]
-      (is (= (:body response) "hello world"))
-      (is (= (:status response) 200)))))
-
-(deftest experiments2
-  (with-router-server myroutes
-    (let [response (http/get (str base-url "/static/test.txt"))]
-      (is (= (:body response) "hello world from test.txt\n"))
-      (is (= (:status response) 200)))))
+  (testing "Chaining handlers."
+    (let [handler1 (fn [ctx]
+                     (rp/delegate ctx {:foo "bar"}))
+          handler2 (fn [ctx]
+                     (let [params (rp/context-params ctx)]
+                       (str "hello " (:foo params))))
+          router (rp/routes [[:get "" handler1 handler2]])]
+      (with-server router
+        (let [response (http/get (str base-url ""))]
+          (is (= (:body response) "hello bar"))
+          (is (= (:status response) 200))))))
+)

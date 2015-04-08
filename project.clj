816138326d8d168f40f43cde78f6afce924f5f36
Andrey Antukh <niwi@niwi.be>
2015-04-08 21:02:03 +0200

    Deactivate unchecked math on development profile (leiningen).

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
                  [org.slf4j/slf4j-simple "1.7.10"]
                  [environ "1.0.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
-  :profiles {:dev {:global-vars {*warn-on-reflection* true
-                                 *unchecked-math* :warn-on-boxed}
+  :profiles {:dev {:global-vars {*warn-on-reflection* true}
+                                 ;; *unchecked-math* :warn-on-boxed}
                    :dependencies [[org.clojure/clojure "1.7.0-alpha6"]
                                   [clj-http "1.1.0"]
                                   [ring/ring-core "1.3.2"

85bc04ae53f85b145f7b2a25de9181f29d3ff010
Andrey Antukh <niwi@niwi.be>
2015-04-24 20:36:26 +0200

    Improved functions for partsing form data.
    Mainly adapting them for run with new context type.

--- src/clojure/catacumba/impl/parse.clj
+++ src/clojure/catacumba/impl/parse.clj
@@ -2,21 +2,28 @@
   "Form parsing facilities."
   (:require [catacumba.utils :as utils])
   (:import ratpack.handling.Context
-           ratpack.form.Form))
+           ratpack.form.Form
+           ratpack.util.MultiValueMap
+           catacumba.impl.context.DefaultContext))
 
 ;; Implementation notes:
 ;; Reflection is used for access to private field of Form instance
 ;; because ratpack does not allows an other way for iter over
 ;; all parameters (including files) in an uniform way.
 
+(defn- extract-files
+  [^Form form]
+  (let [field (.. form getClass (getDeclaredField "files"))]
+    (.setAccessible field true)
+    (.get field form)))
+
 (defn parse-formdata
   "Parse form encoded or multipart request data and return
   a maybe multivalue map."
-  [^Context context]
-  (let [form (.parse context Form)
-        field (.. form getClass (getDeclaredField "files"))
-        _     (.setAccessible field true)
-        files (.get field form)
+  [^DefaultContext context]
+  (let [^Context ctx (:catacumba/context context)
+        ^MultiValueMap form (.parse ctx Form)
+        ^MultiValueMap files (extract-files form)
         result (transient {})]
     (reduce (fn [acc key]
               (let [values (.getAll files key)]

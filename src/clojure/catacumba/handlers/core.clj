08b62eaf7aefab957ca2890829b2232f46f23647
Andrey Antukh <niwi@niwi.be>
2015-05-12 19:23:23 +0200

    Decouple the logic of context population with request params from handler function.

--- src/clojure/catacumba/handlers/core.clj
+++ src/clojure/catacumba/handlers/core.clj
@@ -3,13 +3,21 @@
             [catacumba.impl.handlers :as handlers])
   (:import ratpack.http.Request))
 
+(defn hydrate-context
+  "Populates the context with request related
+  properties."
+  {:no-doc true}
+  [context]
+  (let [^Request request (:request context)]
+    (assoc context
+           :path (str "/" (.. request getPath))
+           :query-string (.. request getQuery)
+           :method (keyword (.. request getMethod getName toLowerCase))
+           :headers (handlers/get-headers request))))
+
 (defn basic-request
   "A chain handler that populates the context
   with a basic request related properties."
   [context]
-  (let [^Request request (:request context)]
-    (->> {:path (str "/" (.. request getPath))
-          :query-string (.. request getQuery)
-          :method (keyword (.. request getMethod getName toLowerCase))
-          :headers (handlers/get-headers request)}
-         (context/delegate context))))
+  (->> (hydrate-context context)
+       (context/delegate context)))

a59ee65e250235a824295e4dc3be72dfcd6c74dd
Andrey Antukh <niwi@niwi.nz>
2015-05-28 09:21:19 +0200

    Add interceptor example.

---
+++ examples/interceptor/project.clj
@@ -0,0 +1,10 @@
+(defproject interceptor "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://github.com/funcool/catacumba"
+  :license {:name "BSD (2-Clause)"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [funcool/catacumba "0.2.0-SNAPSHOT"]]
+  :main ^:skip-aot interceptor.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ examples/interceptor/src/interceptor/core.clj
@@ -0,0 +1,28 @@
+(ns interceptor.core
+  (:require [catacumba.core :as ct]
+            [catacumba.http :as http]
+            [catacumba.handlers :as hs])
+  (:gen-class))
+
+(defn index
+  [context]
+  (http/ok "<strong>Hello World</strong>"
+           {:content-type "text/html; encoding=utg-8"}))
+
+
+(defn- time-interceptor
+  [exc type cont]
+  (let [st (System/nanoTime)]
+    (cont)
+    (let [et (System/nanoTime)
+          elapsed (- et st)
+          elapsed (/ elapsed 1000000000.0)]
+      (println (format "Computation %s elapsed in: %s (sec)" type elapsed)))))
+
+(def app
+  (ct/routes [[:interceptor time-interceptor]
+              [:get index]]))
+
+(defn -main
+  [& args]
+  (ct/run-server app))

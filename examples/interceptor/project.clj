858deee52f61b6a844754421b92708c96467e345
Andrey Antukh <niwi@niwi.nz>
2015-05-30 09:51:19 +0200

    Update catacumba version on interceptor example.

--- examples/interceptor/project.clj
+++ examples/interceptor/project.clj
@@ -4,7 +4,7 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [funcool/catacumba "0.2.0-SNAPSHOT"]]
+                 [funcool/catacumba "0.2.0"]]
   :main ^:skip-aot interceptor.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

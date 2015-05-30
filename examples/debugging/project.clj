14947a939c3dea9b7e2ba18d1ab22dbee7e9473e
Andrey Antukh <niwi@niwi.nz>
2015-05-30 09:50:30 +0200

    Update catacumba version on debugging example.

--- examples/debugging/project.clj
+++ examples/debugging/project.clj
@@ -4,7 +4,7 @@
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [funcool/catacumba "0.1.0-alpha2"]
+                 [funcool/catacumba "0.2.0"]
                  [prone "0.8.1"]]
   :main ^:skip-aot debugging.core
   :target-path "target/%s"

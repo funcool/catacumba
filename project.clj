3de97e5a9f95adfdda80e4c7481cd269ad7e37aa
Andrey Antukh <niwi@niwi.nz>
2015-08-06 19:43:06 +0300

    Update ratpack version to 0.9.19.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
 
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [io.ratpack/ratpack-core "0.9.18"
+                 [io.ratpack/ratpack-core "0.9.19"
                   :exclusions [io.netty/netty-codec-http
                                io.netty/netty-handler
                                io.netty/netty-transport-native-epoll]]

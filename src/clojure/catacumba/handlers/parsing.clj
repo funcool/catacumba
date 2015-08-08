b06360cf0277a6dd58ca95eace0bbf92c778969d
Andrey Antukh <niwi@niwi.nz>
2015-08-08 17:37:05 +0300

    Adapt parsing handlers to the internal catacumba changes.

--- src/clojure/catacumba/handlers/parsing.clj
+++ src/clojure/catacumba/handlers/parsing.clj
@@ -33,8 +33,7 @@
   "A polymorophic method for parse body into clojure
   friendly data structure."
   (fn [^Context ctx ^TypedData body]
-    (let [^Request request (.getRequest ctx)
-          ^String contenttype (.. request getBody getContentType getType)]
+    (let [^String contenttype (.. body getContentType getType)]
       (if contenttype
         (keyword (.toLowerCase contenttype))
         :application/octet-stream)))
@@ -42,11 +41,11 @@
 
 (defmethod parse :multipart/form-data
   [^Context ctx ^TypedData body]
-  (ct/get-formdata ctx))
+  (ct/get-formdata* ctx body))
 
 (defmethod parse :application/x-www-form-urlencoded
   [^Context ctx ^TypedData body]
-  (ct/get-formdata ctx))
+  (ct/get-formdata* ctx body))
 
 (defmethod parse :application/json
   [^Context ctx ^TypedData body]
@@ -67,5 +66,5 @@
   ([parsefn]
    (fn [context]
      (let [^Context ctx (:catacumba/context context)
-           ^TypedData body (.. ctx getRequest getBody)]
-       (ct/delegate context {:body (parsefn ctx body)})))))
+           ^TypedData body (:body context)]
+       (ct/delegate context {:data (parsefn ctx body)})))))

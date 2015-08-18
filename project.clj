cd4badafd6a1ac126de449083fae273e8127d88e
Andrey Antukh <niwi@niwi.nz>
2015-08-18 17:56:30 +0300

    Set version to 0.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/catacumba "0.5.0"
+(defproject funcool/catacumba "0.6.0-SNAPSHOT"
   :description "Asynchronous Web Toolkit for Clojure."
   :url "http://github.com/funcool/catacumba"
   :license {:name "BSD (2-Clause)"
@@ -9,7 +9,7 @@
 
   :jar-exclusions [#"\.swp|\.swo|user.clj"]
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options" "-Xlint:unchecked"]
-
+  :repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [io.ratpack/ratpack-core "0.9.19"

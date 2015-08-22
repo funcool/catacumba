cb228590df858454c90cff04e712a9d3d74a5aa8
Andrey Antukh <niwi@niwi.nz>
2015-08-22 17:12:56 +0300

    Remove slf4j-simple from the required dependencies.

--- project.clj
+++ project.clj
@@ -12,13 +12,13 @@
 
   :repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
+                 [org.slf4j/slf4j-simple "1.7.12" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [io.ratpack/ratpack-core "0.9.19"
                   :exclusions [io.netty/netty-codec-http
                                io.netty/netty-handler
                                io.netty/netty-transport-native-epoll]]
                  [io.netty/netty-all "4.1.0.Beta5"]
-                 [org.slf4j/slf4j-simple "1.7.12"]
                  [cheshire "5.5.0"]
                  [ns-tracker "0.3.0"]
                  [slingshot "0.12.2"]

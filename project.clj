f100839a82b90bd4de230365b08c4c6b64102bfc
Andrey Antukh <niwi@niwi.nz>
2015-10-27 22:26:18 +0200

    Update core.async dependency.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                   "-Xlint:unchecked"]
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.slf4j/slf4j-simple "1.7.12" :scope "provided"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.371"]
                  [io.ratpack/ratpack-core "1.1.1"
                   :exclusions [io.netty/netty-codec-http
                                io.netty/netty-handler

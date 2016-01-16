21182f796baa913633a4d5968b85df33ff369747
Andrey Antukh <niwi@niwi.nz>
2016-01-16 12:22:13 +0200

    Set version to 0.11.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/catacumba "0.10.1"
+(defproject funcool/catacumba "0.11.0"
   :description "Asynchronous Web Toolkit for Clojure."
   :url "http://github.com/funcool/catacumba"
   :license {:name "BSD (2-Clause)"

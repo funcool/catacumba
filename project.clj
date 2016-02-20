e9ee56492c4b9a4eaacb6ce7a5c3dad28128f0f4
Andrey Antukh <niwi@niwi.nz>
2016-02-20 13:40:15 +0200

    Set version to 0.11.2-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/catacumba "0.11.1"
+(defproject funcool/catacumba "0.11.2-SNAPSHOT"
   :description "Asynchronous Web Toolkit for Clojure."
   :url "http://github.com/funcool/catacumba"
   :license {:name "BSD (2-Clause)"

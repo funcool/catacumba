aea4a15379a465b28455f1fe2570cede6b2b92b8
Andrey Antukh <niwi@niwi.nz>
2016-03-20 15:33:28 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -11,15 +11,15 @@
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"
                   "-Xlint:unchecked"]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.slf4j/slf4j-simple "1.7.14" :scope "provided"]
-                 [prone "1.0.2" :scope "provided"]
+                 [org.slf4j/slf4j-simple "1.7.19" :scope "provided"]
                  [org.clojure/core.async "0.2.374"]
-                 [io.ratpack/ratpack-core "1.1.1"
+                 [io.ratpack/ratpack-core "1.2.0"
                   :exclusions [io.netty/netty-codec-http
                                io.netty/netty-handler
                                io.netty/netty-transport-native-epoll]]
-                 [io.netty/netty-all "4.1.0.Beta7"]
-                 [cheshire "5.5.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                 [io.netty/netty-all "4.1.0.Beta8"]
+                 [cheshire "5.5.0"
+                  :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [ns-tracker "0.3.0"]
                  [manifold "0.1.2" :exclusions [riddley]]
                  [com.stuartsierra/component "0.3.1"]

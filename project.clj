e3927e82f0c929378f950e501e0dd5701498d86c
Andrey Antukh <niwi@niwi.nz>
2016-03-22 19:49:31 +0200

    Set 0.13.0-SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/catacumba "0.12.0"
+(defproject funcool/catacumba "0.13.0-SNAPSHOT"
   :description "Asynchronous Web Toolkit for Clojure."
   :url "http://github.com/funcool/catacumba"
   :license {:name "BSD (2-Clause)"

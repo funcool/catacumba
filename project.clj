b725e1429c7d69067359ee55cb4505f3d9f569d5
Andrey Antukh <niwi@niwi.nz>
2016-06-18 09:36:01 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/catacumba "0.17.0"
+(defproject funcool/catacumba "1.0.0-SNAPSHOT"
   :description "Asynchronous Web Toolkit for Clojure."
   :url "http://github.com/funcool/catacumba"
   :license {:name "BSD (2-Clause)"
@@ -12,7 +12,7 @@
                   "-Xlint:unchecked" "-Xlint:deprecation"]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.slf4j/slf4j-simple "1.7.21" :scope "provided"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.2.385"]
                  [io.ratpack/ratpack-core "1.3.3"
                   :exclusions [io.netty/netty-codec-http
                                io.netty/netty-handler
@@ -24,11 +24,11 @@
                  [manifold "0.1.4"]
                  [com.stuartsierra/component "0.3.1"]
                  [commons-io/commons-io "2.5"]
-                 [buddy/buddy-sign "1.0.0"
+                 [buddy/buddy-sign "1.1.0"
                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
 
                  [funcool/cuerdas "0.7.2"]
-                 [funcool/promesa "1.2.0"]
+                 [funcool/promesa "1.3.1"]
                  [danlentz/clj-uuid "0.1.6"]
                  [environ "1.0.3"]
                  [com.cognitect/transit-clj "0.8.285"]])
